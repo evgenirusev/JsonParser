8ce89be01e4450b9c99c7defe3603dd9b66d9d97
rusev9 <evg.rusev@gmail.com>
2019-11-09 15:11:04 +0200

    SB-update

--- app.js
+++ app.js
@@ -1,211 +1,179 @@
-// import { MOCK } from "./MOCK_DATA.js";
-// export type UserMock = {
-//     "avatar": string;
-//     "id": number;
-//     "first_name": string;
-//     "last_name": string;
-//     "email": string;
-//     "gender": string;
-//     "ip_address": string;
-//     "friends": {
-//         "first_name": string;
-//         "last_name": string;
-//     }[];
-// }
-// export class Row {
-//     avatar: string;
-//     id: number;
-//     firstName: string;
-//     lastName: string;
-//     email: string;
-//     gender: string;
-//     IPAddress: string;
-//     friends: Array<Friend>;
-// }
-// export type Friend = {
-//     firstName: string,
-//     lastName: string
-// }
-// class RowParser {
-//     public parseToRow(user: UserMock): Row {
-//         let row: Row = new Row();
-//         row.avatar = user.avatar;
-//         row.email = user.email;
-//         row.firstName = user.first_name;
-//         row.lastName = user.last_name;
-//         row.IPAddress = user.ip_address;
-//         row.friends = user.friends.map(friend => {
-//             return {
-//                 firstName: friend.first_name,
-//                 lastName: friend.last_name 
-//             }
-//         });
-//         row.gender = user.gender;
-//         row.id = user.id;
-//         return row;
-//     }
-// }
-// class DataParser {
-//     private rowBuilder: RowParser;
-//     constructor(rowBuilder: RowParser) {
-//         this.rowBuilder = rowBuilder;
-//     }
-//     public parseData(users: Array<UserMock>): Array<Row> {
-//         return users.map(this.rowBuilder.parseToRow);
-//     }
-// }
-// interface IHtmlElement {
-//     render(): string;
-// }
-// class HTMLElement implements IHtmlElement {
-//     private tag: string;
-//     private value: string;
-//     constructor(tag: string, value: string) {
-//         this.tag = tag;
-//         this.value = value;
-//     }
-//     render() {
-//         return `<${this.tag}>${this.value}</${this.tag}>`;
-//     }
-// }
-// class THTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("th", value);
-//     }
-// }
-// class TDTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("td", value);
-//     }
-// }
-// class TRTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("tr", value);
-//     }
-// }
-// class TableTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("table", value);
-//     }
-// }
-// class ULTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("ul", value);
-//     }
-// }
-// class LITag extends HTMLElement {
-//     constructor(value: string) {
-//         super("li", value);
-//     }
-// }
-// class ImageTag implements IHtmlElement {
-//     private value: string;
-//     private alt?: string;
-//     constructor(value: string, alt?: string) {
-//         this.value = value;
-//         this.alt = alt;
-//     }
-//     render(): string {
-//         return `<img src="${this.value}" ${this.alt ? this.alt : ""}>`
-//     }
-// }
-// class ATag implements IHtmlElement {
-//     private href: string;
-//     private value: string;
-//     constructor(href: string, value: string) {
-//         this.href = href;
-//         this.value = value;
-//     }
-//     render() {
-//         return `<a href="${this.href}">${this.value}</a>`;
-//     }
-// }
-// class EmailTag extends ATag {
-//     constructor(email: string) {
-//         super(`mailto:${email}`, email);
-//     }
-// }
-// class ElementFactory {
-//     private map: Map<string, any>;
-//     constructor() {
-//         this.map = new Map();
-//         this.map.set("th", THTag);
-//         this.map.set("td", TDTag);
-//         this.map.set("tr", TRTag);
-//         this.map.set("table", TableTag);
-//         this.map.set("img", ImageTag);
-//         this.map.set("email", EmailTag);
-//         this.map.set("ul", ULTag);
-//         this.map.set("li", LITag);
-//     }
-//     public create(element: string, value: string) {
-//         return new (this.map.get(element))(value);
-//     }
-// }
-// export class TableUI {
-//     private keys: Array<string>;
-//     private rows: Array<Row>;
-//     private elementFactory: ElementFactory;
-//     private specialElements: Map<string, any>;
-//     constructor(elementFactory: ElementFactory, rows: Array<Row>, keys: Array<string>) {
-//         this.rows = rows;
-//         this.keys = keys;
-//         this.elementFactory = elementFactory;
-//         this.specialElements = new Map();
-//         this.specialElements.set("avatar", this.avatarHandler.bind(this));
-//         this.specialElements.set("email", this.emailHandler.bind(this));
-//         this.specialElements.set("friends", this.friendsHandler.bind(this));
-//     }
-//     private avatarHandler(value: string): string {
-//         return this.elementFactory.create("img", value).render();
-//     }
-//     private emailHandler(value: string): string {
-//         return this.elementFactory.create("email", value).render();
-//     }
-//     private friendsHandler(friends: Array<Friend>): string {
-//         return this.renderUL(
-//             friends.map((friend: Friend) => {
-//                 return this.renderLI(`${friend.firstName} ${friend.lastName}`)
-//             }).join("")
-//         )
-//     }
-//     public render(): string {
-//         return this.renderTable(
-//             this.renderTr(this.keys.map(this.renderTd.bind(this)).join("")) 
-//             + this.rows.map(this.renderRow.bind(this)).join("")
-//         );
-//     }
-//     private renderTable(value: string): string {
-//         return this.elementFactory.create("table", value).render();
-//     }
-//     private renderRow(row: Row): string {
-//         return this.renderTr(this.parseRow(row));
-//     }
-//     private parseRow(row: Row): string {
-//         return this.keys.map(key => {
-//             return this.renderTd(
-//                 this.specialElements.has(key) 
-//                     ? this.specialElements.get(key)(row[key])
-//                     : row[key]
-//             );
-//         }).join("");
-//     }
-//     private renderTr(value: string): string {
-//         return this.elementFactory.create("tr", value).render();
-//     }
-//     private renderTd(value: string): string {
-//         return this.elementFactory.create("td", value).render();
-//     }
-//     private renderUL(value: string): string {
-//         return this.elementFactory.create("ul", value).render();
-//     }
-//     private renderLI(value: string): string {
-//         return this.elementFactory.create("li", value).render();
-//     }
-// }
-// (function(data, document) {
-//     let rows: Array<Row> = new DataParser(new RowParser()).parseData(data);
-//     // todo
-//     let table: TableUI = new TableUI(new ElementFactory(), rows, ["avatar","id","firstName","lastName","email","gender","IPAddress","friends"]);
-//     document.getElementById("app").innerHTML = table.render();
-// }(MOCK.slice(0,10), document))
+import { MOCK } from "./MOCK_DATA.js";
+export class Row {
+}
+class RowParser {
+    parseToRow(user) {
+        let row = new Row();
+        row.avatar = user.avatar;
+        row.email = user.email;
+        row.firstName = user.first_name;
+        row.lastName = user.last_name;
+        row.IPAddress = user.ip_address;
+        row.friends = user.friends.map(friend => {
+            return {
+                firstName: friend.first_name,
+                lastName: friend.last_name
+            };
+        });
+        row.gender = user.gender;
+        row.id = user.id;
+        return row;
+    }
+}
+class DataParser {
+    constructor(rowBuilder) {
+        this.rowBuilder = rowBuilder;
+    }
+    parseData(users) {
+        return users.map(this.rowBuilder.parseToRow);
+    }
+}
+class HTMLElement {
+    constructor(tag, value, attributes) {
+        this.tag = tag;
+        this.value = value;
+        this.attributes = attributes;
+    }
+    render() {
+        return `<${this.tag} ${this.getAttributes()}>${this.value}</${this.tag}>`;
+    }
+    // DRY - reuse this
+    getAttributes() {
+        return this.attributes
+            ? Object.keys(this.attributes).map(key => `${key}=${this.attributes[key]}`).join(" ")
+            : "";
+    }
+}
+class ATag extends HTMLElement {
+    constructor(value, attributes) {
+        super("a", value, attributes);
+    }
+}
+class THTag extends HTMLElement {
+    constructor(value, attributes) {
+        super("th", value, attributes);
+    }
+}
+class TDTag extends HTMLElement {
+    constructor(value, attributes) {
+        super("td", value, attributes);
+    }
+}
+class TRTag extends HTMLElement {
+    constructor(value, attributes) {
+        super("tr", value, attributes);
+    }
+}
+class TableTag extends HTMLElement {
+    constructor(value, attributes) {
+        super("table", value, attributes);
+    }
+}
+class ULTag extends HTMLElement {
+    constructor(value, attributes) {
+        super("ul", value, attributes);
+    }
+}
+class LITag extends HTMLElement {
+    constructor(value, attributes) {
+        super("li", value, attributes);
+    }
+}
+class SingleTagElement {
+    constructor(tag, attributes) {
+        this.tag = tag;
+        this.attributes = attributes;
+    }
+    render() {
+        return `<${this.tag} ${this.getAttributes()}>`;
+    }
+    // create abstraction for this in BaseHTML element !!!!!!
+    getAttributes() {
+        return this.attributes
+            ? Object.keys(this.attributes).map(key => `${key}=${this.attributes[key]}`).join(" ")
+            : "";
+    }
+}
+class ElementFactory {
+    constructor() {
+        this.map = new Map();
+        this.map.set("th", THTag);
+        this.map.set("td", TDTag);
+        this.map.set("tr", TRTag);
+        this.map.set("table", TableTag);
+        this.map.set("ul", ULTag);
+        this.map.set("li", LITag);
+        this.map.set("a", ATag);
+    }
+    create(element, value, attributes) {
+        return new (this.map.get(element))(value, attributes);
+    }
+}
+class SingleTagElementFactory {
+    create(element, attributes) {
+        return new SingleTagElement(element, attributes);
+    }
+}
+export class TableUI {
+    // todo: type
+    constructor(elementFactory, singleTagElementFactory, rows, keys) {
+        this.rows = rows;
+        this.keys = keys;
+        this.elementFactory = elementFactory;
+        this.singleTagElementFactory = singleTagElementFactory;
+        this.elementHandlers = new Map();
+        this.elementHandlers.set("avatar", this.avatarHandler.bind(this));
+        this.elementHandlers.set("email", this.emailHandler.bind(this));
+        this.elementHandlers.set("friends", this.friendsHandler.bind(this));
+    }
+    avatarHandler(value) {
+        return this.singleTagElementFactory.create("img", {
+            src: value
+        }).render();
+    }
+    emailHandler(value) {
+        return this.elementFactory.create("a", value, {
+            href: `mailto:${value}`
+        }).render();
+    }
+    friendsHandler(friends) {
+        return this.renderUL(friends.map((friend) => {
+            return this.renderLI(`${friend.firstName} ${friend.lastName}`);
+        }).join(""));
+    }
+    render() {
+        return this.renderTable(this.renderTr(this.keys.map(this.renderTd.bind(this)).join(""))
+            + this.rows.map(this.renderRow.bind(this)).join(""));
+    }
+    renderTable(value) {
+        return this.elementFactory.create("table", value).render();
+    }
+    renderRow(row) {
+        return this.renderTr(this.parseRow(row));
+    }
+    parseRow(row) {
+        return this.keys.map(key => {
+            return this.renderTd(this.elementHandlers.has(key)
+                ? this.elementHandlers.get(key)(row[key])
+                : row[key]);
+        }).join("");
+    }
+    renderTr(value) {
+        return this.elementFactory.create("tr", value).render();
+    }
+    renderTd(value) {
+        return this.elementFactory.create("td", value).render();
+    }
+    renderUL(value) {
+        return this.elementFactory.create("ul", value).render();
+    }
+    renderLI(value) {
+        return this.elementFactory.create("li", value).render();
+    }
+}
+(function (data, document) {
+    let rows = new DataParser(new RowParser()).parseData(data);
+    // todo
+    let table = new TableUI(new ElementFactory(), new SingleTagElementFactory, rows, ["avatar", "id", "firstName", "lastName", "email", "gender", "IPAddress", "friends"]);
+    document.getElementById("app").innerHTML = table.render();
+}(MOCK.slice(0, 10), document));
--- app.ts
+++ app.ts
@@ -1,248 +1,270 @@
-// import { MOCK } from "./MOCK_DATA.js";
+import { MOCK } from "./MOCK_DATA.js";
 
-// export type UserMock = {
-//     "avatar": string;
-//     "id": number;
-//     "first_name": string;
-//     "last_name": string;
-//     "email": string;
-//     "gender": string;
-//     "ip_address": string;
-//     "friends": {
-//         "first_name": string;
-//         "last_name": string;
-//     }[];
-// }
+export type UserMock = {
+    "avatar": string;
+    "id": number;
+    "first_name": string;
+    "last_name": string;
+    "email": string;
+    "gender": string;
+    "ip_address": string;
+    "friends": {
+        "first_name": string;
+        "last_name": string;
+    }[];
+}
 
-// export class Row {
-//     avatar: string;
-//     id: number;
-//     firstName: string;
-//     lastName: string;
-//     email: string;
-//     gender: string;
-//     IPAddress: string;
-//     friends: Array<Friend>;
-// }
+export class Row {
+    avatar: string;
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    IPAddress: string;
+    friends: Array<Friend>;
+}
 
-// export type Friend = {
-//     firstName: string,
-//     lastName: string
-// }
+export type Friend = {
+    firstName: string,
+    lastName: string
+}
 
-// class RowParser {
-//     public parseToRow(user: UserMock): Row {
-//         let row: Row = new Row();
-//         row.avatar = user.avatar;
-//         row.email = user.email;
-//         row.firstName = user.first_name;
-//         row.lastName = user.last_name;
-//         row.IPAddress = user.ip_address;
-//         row.friends = user.friends.map(friend => {
-//             return {
-//                 firstName: friend.first_name,
-//                 lastName: friend.last_name 
-//             }
-//         });
-//         row.gender = user.gender;
-//         row.id = user.id;
-//         return row;
-//     }
-// }
+class RowParser {
+    public parseToRow(user: UserMock): Row {
+        let row: Row = new Row();
+        row.avatar = user.avatar;
+        row.email = user.email;
+        row.firstName = user.first_name;
+        row.lastName = user.last_name;
+        row.IPAddress = user.ip_address;
+        row.friends = user.friends.map(friend => {
+            return {
+                firstName: friend.first_name,
+                lastName: friend.last_name 
+            }
+        });
+        row.gender = user.gender;
+        row.id = user.id;
+        return row;
+    }
+}
 
-// class DataParser {
-//     private rowBuilder: RowParser;
-//     constructor(rowBuilder: RowParser) {
-//         this.rowBuilder = rowBuilder;
-//     }
+class DataParser {
+    private rowBuilder: RowParser;
+    constructor(rowBuilder: RowParser) {
+        this.rowBuilder = rowBuilder;
+    }
 
-//     public parseData(users: Array<UserMock>): Array<Row> {
-//         return users.map(this.rowBuilder.parseToRow);
-//     }
-// }
+    public parseData(users: Array<UserMock>): Array<Row> {
+        return users.map(this.rowBuilder.parseToRow);
+    }
+}
 
-// interface IHtmlElement {
-//     render(): string;
-// }
+interface IHtmlElement {
+    render(): string;
+}
 
-// class HTMLElement implements IHtmlElement {
-//     private tag: string;
-//     private value: string;
-//     constructor(tag: string, value: string) {
-//         this.tag = tag;
-//         this.value = value;
-//     }
+interface Attributes {
+    [key: string]: string
+}
 
-//     render() {
-//         return `<${this.tag}>${this.value}</${this.tag}>`;
-//     }
-// }
+class HTMLElement implements IHtmlElement {
+    private tag: string;
+    private value: string;
+    private attributes?: Attributes;
 
-// class THTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("th", value);
-//     }
-// }
+    constructor(tag: string, value: string, attributes?: Attributes) {
+        this.tag = tag;
+        this.value = value;
+        this.attributes = attributes;
+    }
 
-// class TDTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("td", value);
-//     }
-// }
+    render() {
+        return `<${this.tag} ${this.getAttributes()}>${this.value}</${this.tag}>`;
+    }
 
-// class TRTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("tr", value);
-//     }
-// }
+    // DRY - reuse this
+    private getAttributes() {
+        return this.attributes 
+            ? Object.keys(this.attributes).map(key => `${key}=${this.attributes[key]}`).join(" ")
+            : "";
+    }
+}
 
-// class TableTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("table", value);
-//     }
-// }
+class ATag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("a", value, attributes);
+    }
+}
 
-// class ULTag extends HTMLElement {
-//     constructor(value: string) {
-//         super("ul", value);
-//     }
-// }
+class THTag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("th", value, attributes);
+    }
+}
 
-// class LITag extends HTMLElement {
-//     constructor(value: string) {
-//         super("li", value);
-//     }
-// }
+class TDTag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("td", value, attributes);
+    }
+}
 
-// class ImageTag implements IHtmlElement {
-//     private value: string;
-//     private alt?: string;
-//     constructor(value: string, alt?: string) {
-//         this.value = value;
-//         this.alt = alt;
-//     }
+class TRTag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("tr", value, attributes);
+    }
+}
 
-//     render(): string {
-//         return `<img src="${this.value}" ${this.alt ? this.alt : ""}>`
-//     }
-// }
+class TableTag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("table", value, attributes);
+    }
+}
 
-// class ATag implements IHtmlElement {
-//     private href: string;
-//     private value: string;
-//     constructor(href: string, value: string) {
-//         this.href = href;
-//         this.value = value;
-//     }
+class ULTag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("ul", value, attributes);
+    }
+}
 
-//     render() {
-//         return `<a href="${this.href}">${this.value}</a>`;
-//     }
-// }
+class LITag extends HTMLElement {
+    constructor(value: string, attributes?: Attributes) {
+        super("li", value, attributes);
+    }
+}
 
-// class EmailTag extends ATag {
-//     constructor(email: string) {
-//         super(`mailto:${email}`, email);
-//     }
-// }
+class SingleTagElement implements IHtmlElement {
+    private tag: string;
+    private attributes?: Attributes;
 
-// class ElementFactory {
-//     private map: Map<string, any>;
-//     constructor() {
-//         this.map = new Map();
-//         this.map.set("th", THTag);
-//         this.map.set("td", TDTag);
-//         this.map.set("tr", TRTag);
-//         this.map.set("table", TableTag);
-//         this.map.set("img", ImageTag);
-//         this.map.set("email", EmailTag);
-//         this.map.set("ul", ULTag);
-//         this.map.set("li", LITag);
-//     }
+    constructor(tag: string, attributes?: Attributes) {
+        this.tag = tag;
+        this.attributes = attributes;
+    }
 
-//     public create(element: string, value: string) {
-//         return new (this.map.get(element))(value);
-//     }
-// }
+    render(): string {
+        return `<${this.tag} ${this.getAttributes()}>`
+    }
 
-// export class TableUI {
-//     private keys: Array<string>;
-//     private rows: Array<Row>;
-//     private elementFactory: ElementFactory;
-//     private specialElements: Map<string, any>;
+    // create abstraction for this in BaseHTML element !!!!!!
+    private getAttributes() {
+        return this.attributes 
+            ? Object.keys(this.attributes).map(key => `${key}=${this.attributes[key]}`).join(" ")
+            : "";
+    }
+}
 
-//     constructor(elementFactory: ElementFactory, rows: Array<Row>, keys: Array<string>) {
-//         this.rows = rows;
-//         this.keys = keys;
-//         this.elementFactory = elementFactory;
-//         this.specialElements = new Map();
-//         this.specialElements.set("avatar", this.avatarHandler.bind(this));
-//         this.specialElements.set("email", this.emailHandler.bind(this));
-//         this.specialElements.set("friends", this.friendsHandler.bind(this));
-//     }
+type Class = { new(...args: any[]): any; };
+class ElementFactory {
+    private map: Map<string, Class>;
+    constructor() {
+        this.map = new Map();
+        this.map.set("th", THTag);
+        this.map.set("td", TDTag);
+        this.map.set("tr", TRTag);
+        this.map.set("table", TableTag);
+        this.map.set("ul", ULTag);
+        this.map.set("li", LITag);
+        this.map.set("a", ATag);
+    }
 
-//     private avatarHandler(value: string): string {
-//         return this.elementFactory.create("img", value).render();
-//     }
+    public create(element: string, value: string, attributes?: Attributes) {
+        return new (this.map.get(element))(value, attributes);
+    }
+}
 
-//     private emailHandler(value: string): string {
-//         return this.elementFactory.create("email", value).render();
-//     }
+class SingleTagElementFactory {
+    public create(element: string, attributes?: Attributes) {
+        return new SingleTagElement(element, attributes);
+    }
+}
 
-//     private friendsHandler(friends: Array<Friend>): string {
-//         return this.renderUL(
-//             friends.map((friend: Friend) => {
-//                 return this.renderLI(`${friend.firstName} ${friend.lastName}`)
-//             }).join("")
-//         )
-//     }
+export class TableUI {
+    private keys: Array<string>;
+    private rows: Array<Row>;
+    private elementFactory: ElementFactory;
+    private singleTagElementFactory: SingleTagElementFactory;
+    private elementHandlers: Map<string, any>;
 
-//     public render(): string {
-//         return this.renderTable(
-//             this.renderTr(this.keys.map(this.renderTd.bind(this)).join("")) 
-//             + this.rows.map(this.renderRow.bind(this)).join("")
-//         );
-//     }
+    // todo: type
+    constructor(elementFactory: ElementFactory, singleTagElementFactory: SingleTagElementFactory, rows: Array<Row>, keys: Array<string>) {
+        this.rows = rows;
+        this.keys = keys;
+        this.elementFactory = elementFactory;
+        this.singleTagElementFactory = singleTagElementFactory;
+        this.elementHandlers = new Map();
+        this.elementHandlers.set("avatar", this.avatarHandler.bind(this));
+        this.elementHandlers.set("email", this.emailHandler.bind(this));
+        this.elementHandlers.set("friends", this.friendsHandler.bind(this));
+    }
 
-//     private renderTable(value: string): string {
-//         return this.elementFactory.create("table", value).render();
-//     }
+    private avatarHandler(value: string): string {
+        return this.singleTagElementFactory.create("img", {
+            src: value
+        }).render();
+    }
 
-//     private renderRow(row: Row): string {
-//         return this.renderTr(this.parseRow(row));
-//     }
+    private emailHandler(value: string): string {
+        return this.elementFactory.create("a", value, {
+            href: `mailto:${value}`
+        }).render();
+    }
 
-//     private parseRow(row: Row): string {
-//         return this.keys.map(key => {
-//             return this.renderTd(
-//                 this.specialElements.has(key) 
-//                     ? this.specialElements.get(key)(row[key])
-//                     : row[key]
-//             );
-//         }).join("");
-//     }
+    private friendsHandler(friends: Array<Friend>): string {
+        return this.renderUL(
+            friends.map((friend: Friend) => {
+                return this.renderLI(`${friend.firstName} ${friend.lastName}`)
+            }).join("")
+        )
+    }
 
-//     private renderTr(value: string): string {
-//         return this.elementFactory.create("tr", value).render();
-//     }
+    public render(): string {
+        return this.renderTable(
+            this.renderTr(this.keys.map(this.renderTd.bind(this)).join("")) 
+            + this.rows.map(this.renderRow.bind(this)).join("")
+        );
+    }
 
-//     private renderTd(value: string): string {
-//         return this.elementFactory.create("td", value).render();
-//     }
+    private renderTable(value: string): string {
+        return this.elementFactory.create("table", value).render();
+    }
 
-//     private renderUL(value: string): string {
-//         return this.elementFactory.create("ul", value).render();
-//     }
+    private renderRow(row: Row): string {
+        return this.renderTr(this.parseRow(row));
+    }
 
-//     private renderLI(value: string): string {
-//         return this.elementFactory.create("li", value).render();
-//     }
-// }
+    private parseRow(row: Row): string {
+        return this.keys.map(key => {
+            return this.renderTd(
+                this.elementHandlers.has(key) 
+                    ? this.elementHandlers.get(key)(row[key])
+                    : row[key]
+            );
+        }).join("");
+    }
 
-// (function(data, document) {
-//     let rows: Array<Row> = new DataParser(new RowParser()).parseData(data);
-//     // todo
-//     let table: TableUI = new TableUI(new ElementFactory(), rows, ["avatar","id","firstName","lastName","email","gender","IPAddress","friends"]);
+    private renderTr(value: string): string {
+        return this.elementFactory.create("tr", value).render();
+    }
+
+    private renderTd(value: string): string {
+        return this.elementFactory.create("td", value).render();
+    }
+
+    private renderUL(value: string): string {
+        return this.elementFactory.create("ul", value).render();
+    }
+
+    private renderLI(value: string): string {
+        return this.elementFactory.create("li", value).render();
+    }
+}
+
+(function(data, document) {
+    let rows: Array<Row> = new DataParser(new RowParser()).parseData(data);
+    // todo
+    let table: TableUI = new TableUI(new ElementFactory(), new SingleTagElementFactory, rows, ["avatar","id","firstName","lastName","email","gender","IPAddress","friends"]);
     
-//     document.getElementById("app").innerHTML = table.render();
-// }(MOCK.slice(0,10), document))
+    document.getElementById("app").innerHTML = table.render();
+}(MOCK.slice(0,10), document))
