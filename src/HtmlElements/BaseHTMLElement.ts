7e021553581ae8c0a46c7726ee5e32debfc7d357
rusev9 <evg.rusev@gmail.com>
2019-11-10 10:39:09 +0200

    SB-update

--- src/HtmlElements/BaseHTMLElement.ts
+++ src/HtmlElements/BaseHTMLElement.ts
@@ -1,9 +1,11 @@
 import { IAttributes, IHtmlElement } from "../Contracts/index";
 
 export abstract class BaseHTMLElement implements IHtmlElement {
+    private defaultValue: string;
     protected attributes?: IAttributes;
     constructor(attributes?: IAttributes) {
         this.attributes = attributes;
+        this.defaultValue = "";
     }
 
     public abstract render(): string;
@@ -11,6 +13,6 @@ export abstract class BaseHTMLElement implements IHtmlElement {
     protected getAttributes() {
         return this.attributes 
             ? Object.keys(this.attributes).map(key => `${key}=${this.attributes[key]}`).join(" ")
-            : "";
+            : this.defaultValue;
     }
 }
